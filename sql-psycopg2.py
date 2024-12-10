7257ecbd94ab5f2a6e3c81b4a96e3a29a7f2d1f4
Deepesh Patel <deepeshpatel28@icloud.com>
2024-12-10 13:34:06 +0000

    Fix connection issue to chinook database

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,39 +1,52 @@
 import psycopg2
 
+try:
+    # Connect to the "chinook" database using the socket path for Gitpod
+    connection = psycopg2.connect(
+        database="chinook",
+        user="gitpod",  # Use 'gitpod' instead of 'postgres'
+        password="",  # No password required for Gitpod
+        host="/home/gitpod/.pg_ctl/sockets",  # Use Gitpod-specific socket path
+        port=""  # Leave port blank since we are using a Unix socket
+    )
 
-# connect to "chinook" database
-connection = psycopg2.connect(database="chinook")
+    # Build a cursor object of the database
+    cursor = connection.cursor()
 
-# build a cursor object of the database
-cursor = connection.cursor()
+    # Query 1 - Select all records from the "Artist" table
+    # cursor.execute('SELECT * FROM "Artist"')
 
-# Query 1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "Artist"')
+    # Query 2 - Select only the "Name" column from the "Artist" table
+    # cursor.execute('SELECT "Name" FROM "Artist"')
 
-# Query 2 - select only the "Name" column from the "Artist" table
-# cursor.execute('SELECT "Name" FROM "Artist"')
+    # Query 3 - Select only "Queen" from the "Artist" table
+    cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
-# Query 3 - select only "Queen" from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+    # Query 4 - Select only by "ArtistId" #51 from the "Artist" table
+    # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
-# Query 4 - select only by "ArtistId" #51 from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+    # Query 5 - Select only the albums with "ArtistId" #51 on the "Album" table
+    # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+    # Query 6 - Select all tracks where the composer is "Queen" from the "Track" table
+    # cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+    # Fetch the results (multiple)
+    results = cursor.fetchall()
 
-# fetch the results (multiple)
-results = cursor.fetchall()
+    # Fetch the result (single)
+    # results = cursor.fetchone()
 
-# fetch the result (single)
-# results = cursor.fetchone()
+except Exception as e:
+    print(f"An error occurred: {e}")
 
-# close the connection
-connection.close()
+finally:
+    try:
+        if connection:
+            connection.close()
+    except NameError:
+        print("Connection was not established, so no need to close it.")
 
-# print results
+# Print results
 for result in results:
     print(result)
